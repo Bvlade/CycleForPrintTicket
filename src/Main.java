b9577d8428e49a57f4925ad733be15121388c569
Admin <vbeloussov@gmail.com>
2020-12-15 10:31:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 200000; i <= 235000; i++) {
+
+            if ((i >= 200000 && i <= 210000) || (i >= 220000 && i <= 235000)) {
+                System.out.println("Ticket-number is: " + i);
+            }
+        }
+    }
+}
